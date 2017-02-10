8714e98ba51a084a5df8f89309fec8e3252d0572
tsalo <tsalo90@gmail.com>
2017-02-10 14:14:43 -0500

    Update week5 HW.

--- week5/slicetiming_motioncorrect_HW.py
+++ week5/slicetiming_motioncorrect_HW.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 
 import os
-import numpy as np
-from nibabel import load
 import nipype.interfaces.fsl as fsl
 import nipype.interfaces.nipy as nipy
 import nipype.interfaces.io as nio
@@ -23,6 +21,8 @@ def pickfirst(func):
 def pickvol(filenames, fileidx, which):
     """
     """
+    import numpy as np
+    from nibabel import load
     if which.lower() == 'first':
         idx = 0
     elif which.lower() == 'middle':
@@ -45,10 +45,12 @@ def get_subs(subject_id, mri_files):
 
 def calc_slicetimes(filenames, TR):
     """
-    TODO: Validate slice order assumption (interleaved ascending) using dicoms.
+    NOTE: Slice order (ascending interleaved) is assumed but not known.
     Create slice order list from nifti image.
     Requires knowledge of slice timing (e.g., interleaved ascending).
     """
+    import numpy as np
+    from nibabel import load
     all_slice_times = []
     for f in filenames:
        n_slices = load(f).get_shape()[2]
@@ -60,8 +62,8 @@ def calc_slicetimes(filenames, TR):
     return all_slice_times
 
 proj_dir = '/home/tsalo006/Music/'
-work_dir = '/scratch/PSB6351/students/salo/week5/'
-sink_dir = '/scratch/PSB6351/students/salo/week5/'
+work_dir = '/scratch/PSB6351_2017/students/salo/working/'
+sink_dir = '/scratch/PSB6351_2017/students/salo/week5/'
 
 sids = ['sub-01'] # ALWAYS TEST WITH ONE PARTICIPANT AND THEN RUN ON FULL SAMPLE
 #sids = ['sub-02', 'sub-03', 'sub-04', 'sub-05',
@@ -73,9 +75,6 @@ motcor_sltimes_wf = pe.Workflow('motcor_sltimes_wf')
 motcor_sltimes_wf.base_dir = work_dir
 
 # Define the outputs for the workflow
-# FEEL FREE TO CHANGE WHAT YOU WANT TO SAVE. 
-# AT A MINIMUM MUST INCLUDE REFERENCE EPI, MOTION CORRECTION PARAMETERS,
-# AND EPI SCANS THAT HAVE BEEN MOTION CORRECTED AND SLICE TIME CORRECTED
 output_fields = ['reference', 'motion_parameters',
                  'motion_sltime_corrected_files']
 outputspec = pe.Node(util.IdentityInterface(fields=output_fields),
@@ -95,12 +94,13 @@ info = dict(mri_files=[['subject_id']])
 datasource = pe.Node(nio.DataGrabber(infields=['subject_id'], outfields=info.keys()), name='datasource')
 datasource.inputs.template = '*_bold.nii.gz'
 datasource.inputs.base_directory = os.path.abspath(proj_dir)
-datasource.inputs.field_template = dict(mri_files='%s/bold.nii.gz')
+datasource.inputs.field_template = dict(mri_files='%s/func/*_bold.nii.gz')
 datasource.inputs.template_args = info
 datasource.inputs.sort_filelist = True
 datasource.inputs.ignore_exception = False
 datasource.inputs.raise_on_empty = True
-motcor_sltimes_wf.connect(subj_iterable, 'subject_id', datasource, 'subject_id')
+motcor_sltimes_wf.connect(subj_iterable, 'subject_id',
+                          datasource, 'subject_id')
 
 # Create a Function node to rename output files with something more meaningful
 getsubs = pe.Node(util.Function(input_names=['subject_id', 'mri_files'],
@@ -112,29 +112,31 @@ motcor_sltimes_wf.connect(subj_iterable, 'subject_id', getsubs, 'subject_id')
 motcor_sltimes_wf.connect(datasource, 'mri_files', getsubs, 'mri_files')
 
 # Extract the first volume of the first run as the reference 
-extractref = pe.Node(fsl.ExtractROI(t_size=1),
+extractref = pe.Node(fsl.ExtractROI(t_size=1, t_min=0),
                      iterfield=['in_file'],
                      name='extractref')
 motcor_sltimes_wf.connect(datasource, ('mri_files', pickfirst),
                           extractref, 'in_file')
-motcor_sltimes_wf.connect(datasource, ('mri_files', pickvol, 0, 'middle'),
-                          extractref, 't_min')
-motcor_sltimes_wf.connect('FOLLOW SYNTAX ABOVE TO CONNECT THIS NODE TO OUTPUTSPEC TO SAVE REFERENCE EPI VOLUME')
+motcor_sltimes_wf.connect(extractref, 'roi_file',
+                          outputspec, 'reference')
 
 # NOTE: Committing to NIPY
 # Simultaneous motion and slice timing correction with Nipy algorithm
 motion_sltime_correct = pe.MapNode(nipy.SpaceTimeRealigner(),
                                    name='motion_sltime_correct',
                                    iterfield = ['in_file', 'slice_times'])
+motcor_sltimes_wf.connect(datasource, ('mri_files', calc_slicetimes, 2.),
+                          motion_sltime_correct, 'slice_times')
 motion_sltime_correct.inputs.tr = 2.
 motion_sltime_correct.inputs.slice_info = 2
-motion_sltime_correct.plugin_args = {'bsub_args': '-n %s' %os.environ['MKL_NUM_THREADS']}
+motion_sltime_correct.plugin_args = {'bsub_args': '-n {0}'.format(os.environ['MKL_NUM_THREADS'])}
 motion_sltime_correct.plugin_args = {'bsub_args': '-R "span[hosts=1]"'}
-motcor_sltimes_wf.connect('FOLLOW SYNTAX TO CONNECT APPROPRIATE NODES TO INPUT IN_FILE')
-motcor_sltimes_wf.connect(datasource, ('mri_files', calc_slicetimes, 2.),
-                          motion_sltime_correct, 'slice_times') # ARE THESE APPROPRIATE?  HOW DO WE FIND OUT?
-motcor_sltimes_wf.connect('WRITE APPROPRIATE SYNTAX TO CONNECT THIS NODE TO OUTPUT TO SAVE MOTION_PARAMETER FILES')
-motcor_sltimes_wf.connect('WRITE APPROPRIATE SYNTAX TO CONNECT THIS NODE TO OUTPUT TO SAVE MOTION AND SLICE TIME CORRECTED EPI FILES')
+motcor_sltimes_wf.connect(datasource, 'mri_files',
+                          motion_sltime_correct, 'in_file')
+motcor_sltimes_wf.connect(motion_sltime_correct, 'par_file',
+                          outputspec, 'motion_parameters')
+motcor_sltimes_wf.connect(motion_sltime_correct, 'out_file',
+                          outputspec, 'motion_sltime_corrected_files')
 
 # Save the relevant data into an output directory
 datasink = pe.Node(nio.DataSink(), name='datasink')
@@ -148,6 +150,8 @@ motcor_sltimes_wf.connect(getsubs, 'subs', datasink, 'substitutions')
 # Run things and write crash files if necessary
 motcor_sltimes_wf.config['execution']['crashdump_dir'] = '/scratch/PSB6351_2017/students/salo/crash/week5_nipype_crash'
 motcor_sltimes_wf.base_dir = work_dir
+
+motcor_sltimes_wf.write_graph(graph2use='exec')
 motcor_sltimes_wf.run(plugin='LSF', plugin_args={'bsub_args': '-q PQ_nbc'})
 
 # To view crash results: nipype_display_crash [crashdump_dir]/[crash_file].pklz
