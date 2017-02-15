94da73131f8725a6c1c1fcf280b5c34ef1eeecdc
Taylor Salo <tsalo006@fiu.edu>
2017-02-15 12:08:56 -0500

    Finish updating structure.
    Command line git is hard.

--- week4/convert_dicoms_shell.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#BSUB -J psb6351_dcm_convert
-#BSUB -o /scratch/PSB6351_2017/week4/salo/crash/dcm_convert_out
-#BSUB -e /scratch/PSB6351_2017/week4/salo/crash/dcm_convert_err
-
-./dicomconvert2_GE.py -d /scratch/PSB6351_2017/dicoms/ -o /scratch/PSB6351_2017/week4/salo/data/ -f /scratch/PSB6351_2017/week4/salo/heuristic_shell.py -q PQ_nbc -c dcm2nii -s subj001
-
--- week4/psb6351-subj001.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python /scratch/PSB6351_2017/week4/salo/dicomconvert2_GE.py -d /scratch/PSB6351_2017/dicoms -o /scratch/PSB6351_2017/week4/salo/data -f /scratch/PSB6351_2017/week4/salo/heuristic_shell.py -s subj001 -c dcm2nii
